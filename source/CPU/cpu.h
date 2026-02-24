57e69477ee49076cb0e3850a98a10d33bf03f2fd
Rab7b <5myinc5@gmail.com>
2026-02-24 17:03:09 -0400

    Refactor CPU class memory management
    Changed memory and totalSize to static members. Added error handling for memory allocation in the constructor.

--- source/CPU/cpu.h
+++ source/CPU/cpu.h
@@ -11,16 +11,19 @@
 
 class CPU {
 private:
-  uint64_t *memory;
+  static uint64_t *memory;
   uint64_t hex_32[12] = {0x31, 0xC0, 0x50, 0x68, 0xB8, 0xCD, 0xBAADF00D, 0xCCCCCCCC, 0xCDCDCDCD, 0xDDDDDDDD, 0xFEEEFEEE, 0xDEADBEEF};
   uint64_t hex_64[11] = {0x90, 0xCC, 0x05, 0xEB, 0xFF, 0xC3, 0x31, 0xCDCDCDCDCDCDCDCD, 0xCCCCCCCCCCCCCCCC, 0xDDDDDDDDDDDDDDDD, 0xFDFDFDFD};
   uint64_t sp;
-  size_t totalSize;
-
+  static size_t totalSize;
 public:
   CPU(size_t ramSize) {
-    totalSize = ramSize;
-    memory = new uint64_t[ramSize]();
+    try{
+      totalSize = ramSize;
+      memory = new uint64_t[ramSize]();
+    }catch (std::bad_alloc){
+      std::cout << "[CPU]: Not enough memory" << std::endl;
+    }
     sp = (ramSize > 0xFFFF) ? 0xFFFF : (uint64_t)(ramSize - 1);
   }
 
